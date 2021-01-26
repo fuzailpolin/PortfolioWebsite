6ef1ade367ed2e38b67c8c745a558ca0e86d666a
fuzailpolin <fuzail.polin@gmail.com>
2021-01-26 18:40:27 +0600

    responsive menu bar

--- assets/script.js
+++ assets/script.js
@@ -8,4 +8,11 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
     })
+
+    //toggle menu/navbar script
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    })
 });
