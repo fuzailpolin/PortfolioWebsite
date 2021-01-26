4105d49b4c062797f7cac768de5cf8af9da7e866
fuzailpolin <fuzail.polin@gmail.com>
2021-01-26 17:21:51 +0600

    sticky Header

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+
+// navbar sticky funtion
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    })
+});
